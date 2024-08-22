3f0c63d6249e88fb8c676cea18ae860711431e2f
sohyunpark <33.sohyun.park@gmail.com>
2024-08-22 16:33:48 +0900

    14일차 1번 풀이

---
+++ 기초/14_01_홀수vs짝수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let even = 0, odd = 0;
+
+    for(let i = 0; i < num_list.length; i++){
+        if(i % 2 === 0){
+            odd += num_list[i];
+        } else {
+            even += num_list[i];
+        }
+    }
+
+    return even > odd ? even : odd
+}
