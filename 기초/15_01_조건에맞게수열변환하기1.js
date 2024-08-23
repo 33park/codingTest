c13ed4a4ac6fc35927f7b3fd5729d09ccf9c6861
sohyunpark <33.sohyun.park@gmail.com>
2024-08-23 17:24:51 +0900

    15일차 1번 풀이

---
+++ 기초/15_01_조건에맞게수열변환하기1.js
@@ -0,0 +1,31 @@
+/**
+ * map?
+ */
+function solution(arr){ 
+    return arr.map(num => {
+        if(num >= 50 && !(num % 2)) return num / 2;
+        if(num < 50 && num % 2) return num * 2;
+
+        return num
+    })
+}
+solution([1, 2, 3, 100, 99, 98]);
+
+// function solution01(arr) {
+
+//     let res = [];
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] >= 50 && arr[i] % 2 === 0) {
+// 			arr[i] = arr[i] / 2;
+// 		} else if (arr[i] < 50 && arr[i] % 2 != 0) {
+// 			arr[i] = arr[i] * 2;
+// 		}
+
+//         res.push(arr[i])
+// 	}
+
+//     return res
+// }
+
+// solution([1, 2, 3, 100, 99, 98]);
