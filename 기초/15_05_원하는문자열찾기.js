c6ddda9b69c2b37dadcf1712a0dcc0ec3a7c8ae9
sohyunpark <33.sohyun.park@gmail.com>
2024-08-28 14:14:29 +0900

    15일자 5번, 16일차 1~ 2번 풀이

--- 기초/15_05_원하는문자열찾기.js
+++ 기초/15_05_원하는문자열찾기.js
@@ -1,15 +1,3 @@
-/**
- * 길이체크
- */
-
-function solution(num_list) {
-	if (num_list.length >= 11) {
-		return num_list.reduce((acc, cur) => acc + cur);
-	} else {
-		return num_list.reduce((acc, cur) => acc * cur);
-	}
+function solution(myString, pat) {
+    return myString.toLocaleLowerCase().includes(pat.toLocaleLowerCase()) ? 1 : 0;
 }
-
-//더 간략하게
-
-const solution02 = (n) => n.reduce((a, v) => (n.length > 10 ? a + v : a * v));
