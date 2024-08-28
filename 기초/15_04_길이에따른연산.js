520bce43fcc819bdb707acb865bf385c87ea0692
sohyunpark <33.sohyun.park@gmail.com>
2024-08-28 13:27:43 +0900

    15일차 3번 풀이

--- 기초/15_04_길이에따른연산.js
+++ 기초/15_04_길이에따른연산.js
@@ -6,27 +6,26 @@
 function solution(num_list) {
 	let count = 0;
 
-    for(let i = 0; i < num_list.length; i++){
-        let num = num_list[i];
-        while(num != 1 ){
-            if (num % 2 == 0 ){
-                num /= 2;
-            } else {
-                num = (num - 1) / 2;
-            }
-            count++;
-        }
-    }
+	for (let i = 0; i < num_list.length; i++) {
+		let num = num_list[i];
+		while (num != 1) {
+			if (num % 2 == 0) {
+				num /= 2;
+			} else {
+				num = (num - 1) / 2;
+			}
+			count++;
+		}
+	}
 
-   return count
+	return count;
 }
 
 solution([12, 4, 15, 1, 14]);
-
 
 /**
  * 다른 사람의 풀이
  * toString(2)의 이진수로 바꾸는 방법도 있으나...
  * 확장성과 가독성이 부족하다
- * 
-*/
+ *
+ */
