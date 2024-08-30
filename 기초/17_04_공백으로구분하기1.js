867364a2d6f565ac35ddbaee0d731a93a26fed27
sohyunpark <33.sohyun.park@gmail.com>
2024-08-30 16:08:58 +0900

    17일차 4,5번 풀이

--- 기초/17_04_공백으로구분하기1.js
+++ 기초/17_04_공백으로구분하기1.js
@@ -1,23 +1,3 @@
-function solution(strArr) {
-	strArr = strArr.filter((x) => {
-		// console.log(x.indexOf("ad"))
-		return x.indexOf("ad") == -1; //일치하는 값이 없으면 -1을 반환합니다.
-	});
-
-	return strArr;
-}
-
-solution(["and", "notad", "abcd"]);
-
-function solution01(strArr) {
-	let res = [];
-
-	strArr.forEach((ele) => {
-		let condition = ele.includes("ad");
-		if (!condition) {
-			res.push(ele);
-		}
-	});
-
-	return res;
+function solution(my_string) {
+    return my_string.split(" ")
 }
---
+++ 기초/18_01_x사이의개수.js
@@ -0,0 +1,37 @@
+/**
+ * 한칸 공백으로 arr 만들고
+ * forEeach로 공백요소 삭제
+ */
+function solution(my_string) {
+	let arr = my_string.split(" ");
+	arr = arr.filter((str) => {
+		return str.length > 0;
+	});
+
+	return arr;
+}
+
+solution(" i    love  you");
+solution("    programmers  ");
+
+//다른사람 풀이
+function solution01(my_string) {
+	return my_string.split(" ").filter((v) => v);
+	/**
+     * `v => v`에서 v는 배열의 각 요소를 의미하며, 자바스크립트에서는 조건문에서 아래와 같은 값들이 false로 평가
+     * 
+
+        false
+        0
+        "" (빈 문자열)
+        null
+        undefined
+        NaN
+        
+    *   이외의 값들은 true로 평가
+    */
+}
+
+function solution02(my_string) {
+	return my_string.trim().split(/ +/);
+}
