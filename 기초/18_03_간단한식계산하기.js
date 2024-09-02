2f356263db2f153d5be0c6267ceffa6c3f3a8512
sohyunpark <33.sohyun.park@gmail.com>
2024-09-02 11:09:13 +0900

    18일차 3번 풀이

--- 기초/18_03_간단한식계산하기.js
+++ 기초/18_03_간단한식계산하기.js
@@ -1,4 +1,25 @@
+//다른 사람 풀이 참고. 이렇게 작성하고 싶었다...!
+const ops = {
+	"+": (a, b) => a + b,
+	"-": (a, b) => a - b,
+	"*": (a, b) => a * b,
+};
+
 function solution(binomial) {
-    var answer = 0;
-    return answer;
+    const [a, op, b] = binomial.split(' ');
+    return ops[op](+a, +b);
+} 
+
+
+
+function solution01(binomial) {
+	let [a, calculate, b] = binomial.split(" ");
+
+	if (calculate == "+") {
+		return Number(a) + Number(b);
+	} else if (calculate == "-") {
+		return Number(a) - Number(b);
+	} else if (calculate == "*") {
+		return Number(a) * Number(b);
+	}
 }
---
+++ 기초/18_04_문자열바꿔서찾기.js
@@ -0,0 +1,4 @@
+function solution(myString, pat) {
+    var answer = 0;
+    return answer;
+}
