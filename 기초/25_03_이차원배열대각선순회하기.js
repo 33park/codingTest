8e6b86185780d8fef04dabff4c6e2418d290c389
sohyunpark <33.sohyun.park@gmail.com>
2024-10-17 17:01:06 +0900

    [박소현] 25일차 마지막 풀이 진행중

--- 기초/25_03_이차원배열대각선순회하기.js
+++ 기초/25_03_이차원배열대각선순회하기.js
@@ -1,4 +1,30 @@
 function solution(board, k) {
-    var answer = 0;
-    return answer;
+	//i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 solution 함수를 완성해 주세요.
+
+	let res = 0;
+
+    for(let i = k; i > 0; i--){
+        for(let j = 0; j < k - i ; j++){
+            if(i + j <= k) {
+                res += board[i][j]
+            }
+        }
+    }
+
+	return res;
 }
+
+solution(
+	[
+		[0, 1, 2],
+		[1, 2, 3],
+		[2, 3, 4],
+		[3, 4, 5],
+	],
+	2
+); //8
+
+
+/**
+ * 1회차- 런타임 에러 발생, for문에 for가 중복되어 생긴것으로 보임
+*/
